1439042652083320d3a87b555640c6e7064ffb51
Pete Codes <38405508+pete-codes@users.noreply.github.com>
2021-09-20 10:58:07 +0100

    Added colors for black text

---
+++ Accessible Colors/accessibleColors.js
@@ -0,0 +1,92 @@
+
+
+function redSolution() {
+    document.getElementById("red").style.backgroundColor="#a70505";
+    document.getElementById("red").style.border="2px solid #a70505";
+    document.getElementById("red").style.color="#ffffff";
+    document.getElementById("red").style.fontSize="22px";
+    document.getElementById("red").innerHTML="Accessible red with white text #a70505";
+    //select the text in the text box
+    h2.select();
+     //copy the text to the clipboard
+     document.execCommand("copy");
+    
+}
+
+function blueSolution() {
+    document.getElementById("blue").style.backgroundColor="blue";
+    document.getElementById("blue").style.border="2px solid blue";
+    document.getElementById("blue").style.color="#ffffff";
+   
+    document.getElementById("blue").innerHTML="Accessible blue with white text #jfk3kj";
+}
+
+function greenSolution() {
+    document.getElementById("green").style.backgroundColor="#11571a";
+    document.getElementById("green").style.color="#11571a";
+    document.getElementById("green").style.border="2px solid #11571a";
+    document.getElementById("green").style.color="#ffffff";
+   
+    document.getElementById("green").innerHTML="Accessible green with white text #g3j3j";
+}
+
+function purpleSolution() {
+    document.getElementById("purple").style.backgroundColor="#5c1c9e";
+    document.getElementById("purple").style.color="#5c1c9e";
+    document.getElementById("purple").style.border="2px solid #5c1c9e";
+    document.getElementById("purple").style.color="#ffffff";
+   
+    document.getElementById("purple").innerHTML="Accessible purple with white text #5c1c9e";
+}
+
+function blackSolution() {
+    document.getElementById("black").style.backgroundColor="#070707";
+    document.getElementById("black").style.color="#070707";
+    document.getElementById("black").style.border="2px solid #070707";
+    document.getElementById("black").style.color="#ffffff";
+   
+    document.getElementById("black").innerHTML="Accessible black with white text #070707";
+}
+
+function pinkSolution() {
+    document.getElementById("pink").style.backgroundColor="#ffc8c8";
+    
+    document.getElementById("pink").style.border="2px solid #ffc8c8";
+    document.getElementById("pink").style.color="000000";
+   
+    document.getElementById("pink").innerHTML="Accessible pink with black text #ffc8c8";
+}
+
+function blueBlackSolution() {
+    document.getElementById("blueBlack").style.backgroundColor="#9bd5ff";
+    
+    document.getElementById("blueBlack").style.border="2px solid #9bd5ff";
+    document.getElementById("blueBlack").style.color="000000";
+   
+    document.getElementById("blueBlack").innerHTML="Accessible blue with black text #9bd5ff";
+}
+
+function greenBlackSolution() {
+    document.getElementById("greenBlack").style.backgroundColor="#35bd1f";
+    
+    document.getElementById("greenBlack").style.border="2px solid #35bd1f";
+    document.getElementById("greenBlack").style.color="000000";
+   
+    document.getElementById("greenBlack").innerHTML="Accessible green with black text #35bd1f";
+}
+
+function yellowSolution() {
+    document.getElementById("yellow").style.backgroundColor="#ffe764";
+    
+    document.getElementById("yellow").style.border="2px solid #ffe764";
+    document.getElementById("yellow").style.color="000000";
+   
+    document.getElementById("yellow").innerHTML="Accessible yellow with black text #ffe764";
+}
+
+
+
+
+
+
+
